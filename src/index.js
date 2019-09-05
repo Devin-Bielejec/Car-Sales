4f179bc310e3e9f6bf86615a902fe2d2462b3505
devin-bielejec <devin.bieleje@gmail.com>
2019-09-05 19:05:55 -0400

    Got header to get data from redux store and render

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { reducer } from "./reducers";
 
-const store = createStore(reducer);
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+
+const store = createStore(reducer);
 
 const rootElement = document.getElementById('root');
 ReactDOM.render
